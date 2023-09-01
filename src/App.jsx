eb2d75cc7024425f16f66fca20ea495fd5747f7d
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-31 22:16:34 -0300

    client side routing <Link />

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ const App = () => {
       <h1>Hello from the main page of the app!</h1>
       <p>Here are some examples of links to other pages</p>
       <nav>
-        <Link to="/profile/popeye">Popeye page</Link>
-        <br />
-        <Link to="/profile/spinach">Spinach page</Link>
+      <ul>
+        <li><Link to="profile">Profile page</Link></li>
+      </ul>
       </nav>
     </div>
   );
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,22 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import Profile from './components/Profile'
-import ErrorPage from "./components/ErrorPage";
+import Profile from "./Profile";
+import ErrorPage from "./ErrorPage";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
+      errorElement: <ErrorPage />,
     },
     {
-      path: "/profile/:name",
+      path: "profile/:name",
       element: <Profile />,
     },
-    {
-        path: "*",
-        element: <ErrorPage />,
-      },
   ]);
 
   return <RouterProvider router={router} />;
