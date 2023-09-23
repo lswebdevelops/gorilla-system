1283c141c4c0e0ac61ee3d4ea3da3778d30b7dd1
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-09-22 21:39:09 -0300

    recreating home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,56 @@
 import "../styles/Home.css";
-import "../assets/data/investments";
 
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [investments, setInvestments] = useState([]);
-  useEffect(() => {
-    fetch("/src/assets/data/investments.json")
-      .then((res) => res.json())
-      .then((data) => setInvestments(data.investments));
-  }, []);
-
-  const investmentElements = investments.map((investment) => (
-    <Link to={`/${investment.id}`} key={investment.id}>
-      <div className="investiment-types-img-container-intern-div">
-        <img src={`${investment.image_link}`} alt={investment.type} />
-        <p>{investment.type}</p>
-      </div>
-    </Link>
-  ));
-
   return (
     <div>
       <div className="investiment-types-img-container">
-        {investmentElements}
+        <Link to={`/1`} key="1">
+          <div className="investiment-types-img-container-intern-div">
+            <img
+              src="https://raw.githubusercontent.com/lswebdevelops/gorilla-system/main/src/assets/images/stocks-image.jpg"
+              alt="stocks"
+            />
+            <p>Stocks</p>
+          </div>
+        </Link>
+
+        
+        <Link to={`/2`} key="2">
+          <div className="investiment-types-img-container-intern-div">
+            <img
+              src="https://raw.githubusercontent.com/lswebdevelops/gorilla-system/main/src/assets/images/reits-image.jpg"
+              alt="reits"
+            />
+            <p>Reits</p>
+          </div>
+        </Link>
+        <Link to={`/3`} key="3">
+          <div className="investiment-types-img-container-intern-div">
+            <img
+              src="https://raw.githubusercontent.com/lswebdevelops/gorilla-system/main/src/assets/images/bonds-image.jpg"
+              alt="bonds"
+            />
+            <p>Bonds</p>
+          </div>
+        </Link>
+        <Link to={`/4`} key="4">
+          <div className="investiment-types-img-container-intern-div">
+            <img
+              src="https://raw.githubusercontent.com/lswebdevelops/gorilla-system/main/src/assets/images/emergency-fund-image.jpg"
+              alt="emergency-fund"
+            />
+            <p>Emergency Fund</p>
+          </div>
+        </Link>
+
+       
+
+      
+
+
       </div>
     </div>
   );
