eac3b7609ff11e5726f18025cf0c9e827c2f5b09
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-10-03 15:12:30 -0300

    adusting pages to deploy

--- src/pages/gorillasgram_pages/Gorillasgram.jsx
+++ src/pages/gorillasgram_pages/Gorillasgram.jsx
@@ -2,7 +2,7 @@ const Gorillasgram = () => {
     return (
         <>
         <h1>Gorillasgram</h1>
-        <p>Criar o 'Gorillasgram' onde que o user pode criar seus próprios memes(página nova pra isso) e salvá-los nessa página</p>
+        <p>Criar o Gorillasgram onde que o user pode criar seus próprios memes, página nova pra isso, e salvá-los nessa página</p>
         </>
     )
 }
