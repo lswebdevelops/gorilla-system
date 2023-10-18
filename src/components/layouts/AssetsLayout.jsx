a3fb67cfd966d406ca5bd59973eb6d283a86dbf9
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-10-18 18:36:03 -0300

    adjusting assets navbar

--- src/components/layouts/AssetsLayout.jsx
+++ src/components/layouts/AssetsLayout.jsx
@@ -3,13 +3,13 @@ import { NavLink, Outlet } from "react-router-dom";
 const assetsLayout = () => {
   return (
     <>
-      <nav>
+      <nav className="assets-nav-bar">
         {/* <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets"}>Assets</NavLink> */}
+        <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets/realestate"}>Real Estate</NavLink>
         <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets/bonds"}>Bonds</NavLink>
         <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets/assetnotes"}>Notes</NavLink>
         <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets/emergencyfund"}>Emergency</NavLink>
         <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets/fixedincome"}>Fixed Income</NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets/realestate"}>Real Estate</NavLink>
         <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets/reits"}>REITs</NavLink>
         <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets/stocks"}>Stocks</NavLink>
         <NavLink className={({ isActive }) => (isActive ? "link-active" : null)} to={"/assets/valuereserve"}>Reserve of Value</NavLink>
