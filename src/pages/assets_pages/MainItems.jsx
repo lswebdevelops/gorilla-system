0a90884702c26d20998e630b7a627b1d7b1dc477
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-10-24 21:56:39 -0300

    adding buttons to main-items page to filter url

--- src/pages/assets_pages/MainItems.jsx
+++ src/pages/assets_pages/MainItems.jsx
@@ -1,17 +1,45 @@
-import { Link } from "react-router-dom";
-import stocksData from "../../assets/data/Data10Stocks";
-import bondsData from "../../assets/data/Data10Bonds";
-import reitsData from "../../assets/data/Data10REITs";
+import { Link, NavLink, useSearchParams } from "react-router-dom";
+import stocksData from "../../assets/data/StockBondsREITs";
 import "../../styles/MainItems.css";
+
 const MainItems = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
+//   console.log(typeFilter);
+
+  const displayedElements = typeFilter
+    ? stocksData.filter(
+        (investment) => investment.type.toLocaleLowerCase() === typeFilter
+      )
+    : stocksData;
+
+  const investmentElements = displayedElements.map((investment, index) => (
+    <>
+      <tr key={index}>
+        <td>{investment.ticker}</td>
+        <td>{investment.name}</td>
+        <td>{investment.sector}</td>
+        <td>{investment.type}</td>
+        <td className="td-description">{investment.description}</td>
+        <td>
+          <Link className="link-main-items" to={`${investment.website}`}>
+            {investment.website}
+          </Link>
+        </td>
+      </tr>
+    </>
+  ));
+
   return (
     <div className="divTable-main-items-container">
       <h1>
         Here you will find the ten most important (in my opnion) stocks, bonds,
         REATs, etc
-      </h1>
-      <div className="divTable-main-items">{/* start stocks */}
-        <h2>Stocks</h2>
+      </h1>     
+      <NavLink className="link-is-active link-main-items" to={"/assets/mainitems?="}>Clear</NavLink>
+      <NavLink className="link-is-active link-main-items"  to={"/assets/mainitems?type=bonds"}>Bonds</NavLink>
+      <NavLink className="link-is-active link-main-items"  to={"/assets/mainitems?type=reits"}>REITs</NavLink>
+      <NavLink className="link-is-active link-main-items"  to={"/assets/mainitems?type=stocks"}>Stocks</NavLink>
         <table>
           <thead>
             <tr>
@@ -19,77 +47,12 @@ const MainItems = () => {
               <th>Name</th>
               <th>Sector</th>
               <th>Website</th>
-            </tr>
-          </thead>
-          <tbody>
-            {stocksData.map((stock) => (
-              <tr key={stock.id}>
-                <td>{stock.ticker}</td>
-                <td>{stock.name}</td>
-                <td>{stock.sector}</td>
-                <td>
-                  <Link className="link-main-items" to={`${stock.website}`}>
-                    {stock.website}
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>{" "}
-      {/* end stocks */}
-      <div className="divTable-main-items">{/* start REITs */}
-        <h2>REITs</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Ticker</th>
-              <th>Name</th>
-              <th>Size</th>
-              <th>Website</th>
-            </tr>
-          </thead>
-          <tbody>
-            {reitsData.map((reits) => (
-              <tr key={reits.id}>
-                <td>{reits.ticker}</td>
-                <td>{reits.name}</td>
-                <td>{reits.size}</td>
-                <td>
-                  <Link className="link-main-items" to={`${reits.website}`}>
-                    {reits.website}
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>{" "}
-      {/* end REITs */}
-      <div className="divTable-main-items">{/* start bonds */}
-        <h2>Bonds</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Issuer</th>
-              <th>Name</th>
-              <th>Maturity Date</th>
+              <th>Type</th>
               <th>Description</th>
             </tr>
           </thead>
-          <tbody>
-            {bondsData.map((bonds) => (
-              <tr key={bonds.id}>
-                <td>{bonds.issuer}</td>
-                <td>{bonds.name}</td>
-                <td>{bonds.maturityDate}</td>
-                <td>{bonds.description}</td>
-              </tr>
-            ))}
-          </tbody>
+          <tbody>{investmentElements}</tbody>
         </table>
-      </div> {/* end bonds */}
-     
     </div>
   );
 };
